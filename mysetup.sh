d8be23667b97791268b41d00df76f5a08c2e7600
Jcodex <misha1994g@yandex.ru>
2020-02-29 13:39:22 +0300

    Add files via upload

--- mysetup.sh
+++ mysetup.sh
@@ -4,5 +4,11 @@ mkdir /home/vagrant/ansible
 cd /home/vagrant/ansible
 cp /vagrant/play.yaml .
 ansible-playbook play.yaml
-sudo cp -r /vagrant/jenkins/jobs /var/lib/jenkins
+sudo cp -r /vagrant/jenkins /var/lib
+sudo chown jenkins -R /var/lib/jenkins
+mkdir /home/vagrant/build2job
+cp /vagrant/build.xml /home/vagrant/build2job
+cp /vagrant/checkstyle-8.29-all.jar /home/vagrant/build2job
+cp /vagrant/sun_checks.xml /home/vagrant/build2job
+sudo chmod 777 -R /home/vagrant/build2job
 sudo service jenkins restart
